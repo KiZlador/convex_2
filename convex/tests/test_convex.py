036386672aa6412e04a2c2967508082d09fa0351
KiZlador <72183235+KiZlador@users.noreply.github.com>
2023-11-02 01:40:49 +0300

    Add files via upload

--- convex/tests/test_convex.py
+++ convex/tests/test_convex.py
@@ -2,12 +2,13 @@ from pytest import approx
 from math import sqrt
 from r2point import R2Point
 from convex import Figure, Void, Point, Segment, Polygon
-
+from modification import Circle_perimeter
 
 class TestVoid:
 
     # Инициализация (выполняется для каждого из тестов класса)
     def setup_method(self):
+        self.d = Circle_perimeter(0, 0, 0)
         self.f = Void()
 
     # Нульугольник является фигурой
@@ -28,14 +29,15 @@ class TestVoid:
 
     # При добавлении точки нульугольник превращается в одноугольник
     def test_add(self):
-        assert isinstance(self.f.add(R2Point(0.0, 0.0)), Point)
+        assert isinstance(self.f.add(R2Point(0.0, 0.0), self.d), Point)
 
 
 class TestPoint:
 
     # Инициализация (выполняется для каждого из тестов класса)
     def setup_method(self):
-        self.f = Point(R2Point(0.0, 0.0))
+        d = Circle_perimeter(0, 0, 0)
+        self.f = Point(R2Point(0.0, 0.0), d)
 
     # Одноугольник является фигурой
     def test_figure(self):
@@ -66,7 +68,8 @@ class TestSegment:
 
     # Инициализация (выполняется для каждого из тестов класса)
     def setup_method(self):
-        self.f = Segment(R2Point(0.0, 0.0), R2Point(1.0, 0.0))
+        d = Circle_perimeter(0, 0, 0)
+        self.f = Segment(R2Point(0.0, 0.0), R2Point(1.0, 0.0), d)
 
     # Двуугольник является фигурой
     def test_figure(self):
@@ -101,11 +104,12 @@ class TestPolygon:
 
     # Инициализация (выполняется для каждого из тестов класса)
     def setup_method(self):
+        d = Circle_perimeter(0, 0, 0)
         self.f = Polygon(
             R2Point(
                 0.0, 0.0), R2Point(
                 1.0, 0.0), R2Point(
-                0.0, 1.0))
+                0.0, 1.0), d)
 
     # Многоугольник является фигурой
     def test_figure(self):
